be2ddfdb524a726af1b8094c366e9b70c64d841a
arcadeVault <neoarcadegame0@gmail.com>
2024-12-30 18:00:06 +0530

    added the logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h3>My name is Arun&Allen Pal Singh and i am living in India</h3>
+      <img src={logo} alt="logo" />
     </div>
   );
 }
