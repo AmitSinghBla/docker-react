56d94c246f14c5b4f1647fd4d3b5607647485d14
arcadeVault <neoarcadegame0@gmail.com>
2024-12-30 17:19:51 +0530

    updated code

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/My name is Amit Pal Singh and i am living in India/i);
+  const linkElement = screen.getByText(/My name is Arun&Allen Pal Singh and i am living in India/i);
   expect(linkElement).toBeInTheDocument();
 });
 
